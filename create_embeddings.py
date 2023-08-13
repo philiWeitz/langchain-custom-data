ec1053c08cee86464a4f6dd70a2fa4940098d5c4
Philipp <philiweitz@gmail.com>
2023-08-13 16:34:06 +0300

    Add confluence loader support

--- create_embeddings.py
+++ create_embeddings.py
@@ -4,7 +4,7 @@ import pickle
 from pydash import head, tail
 
 from config import EMBEDDINGS_FILE
-from langchain.document_loaders import UnstructuredHTMLLoader
+from langchain.document_loaders import ConfluenceLoader, UnstructuredHTMLLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import FAISS
@@ -34,4 +34,32 @@ def get_from_local_knowledge_base():
         pickle.dump(vector_store, f)
 
 
+# https://llamahub.ai/l/confluence
+def get_from_confluence():
+    confluence_loader = ConfluenceLoader(
+        url=os.getenv("CONFLUENCE_URL"),
+        api_key=os.getenv("CONFLUENCE_API_TOKEN"),
+        username=os.getenv("CONFLUENCE_USER_NAME"),
+        max_retry_seconds=1,
+    )
+
+    documents = confluence_loader.load(
+        page_ids=["11111111"],
+        include_attachments=False,
+        include_archived_content=False,
+        include_comments=False,
+        max_pages=5,
+    )
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(texts, embeddings)
+
+    with open(EMBEDDINGS_FILE, "wb") as f:
+        pickle.dump(vector_store, f)
+
+
 get_from_local_knowledge_base()
+# get_from_confluence()
